b339560cf3b40027557cf8d88f8c21b1ce6b289a
amitdesh <amitdesh2014@gmail.com>
2020-07-20 14:45:37 -0400

    Completed exercise

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,53 @@
 class Startup
 
+    attr_accessor :name, :founder, :domain
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(name, domain_str)
+        self.name = name
+        self.domain = domain_str
+        self
+    end
+
+    def self.find_by_founder(founder_name)
+        self.all.find{|startup| startup.founder == founder_name}
+    end
+
+    def self.domains
+        self.all.map{|startup| startup.domain}.uniq
+    end
+
+    def sign_contract(vc_object, type, inv_amt)
+        FundingRound.new(self, vc_object,type,inv_amt)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.count{|round| round.startup == self}
+    end
+
+    def total_funds
+        FundingRound.all.map{|round| round.investment if round.startup == self}.compact.sum
+    end
+
+    def investors
+        FundingRound.all.map{|round| round.venture_capitalist if round.startup == self}.compact.uniq
+    end
+
+    def big_investors
+        FundingRound.all.map{|round| round.venture_capitalist if (VentureCapitalist.tres_comma_club.include?(round.venture_capitalist) && round.startup == self)}.compact.uniq
+    end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup1 = Startup.new("Snapchat","Evan","Social Media")
+startup2 = Startup.new("Facebook", "Zuck","Social Media")
+startup3 = Startup.new("Google","Zuck", "Search")
+startup4 = Startup.new("Amazon","Bezos", "Ecommerce")
+
+vc1 = VentureCapitalist.new("TCV",1200000000)
+vc2 = VentureCapitalist.new("Sequoia",2000000000)
+vc3 = VentureCapitalist.new("Battery",500000000)
+vc4 = VentureCapitalist.new("Cowboy",200000000)
+
+fr1 = FundingRound.new(startup1,vc1,"Seed",100)
+fr2 = FundingRound.new(startup1,vc2,"Growth",500)
+fr3 = FundingRound.new(startup1,vc1,"Seed",400)
+fr4 = FundingRound.new(startup2,vc3,"Series A",250)
+fr5 = FundingRound.new(startup3,vc2,"Seed",100)
+fr6 = FundingRound.new(startup4,vc4,"Series A",200)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
